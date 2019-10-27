7b595aed78c6d593ed561e0915eb1a634fd72f0e
Phong Ha <realphongha@gmail.com>
2019-10-27 11:59:31 +0700

    Skeleton project
    Xương project, sử dụng Maven.

---
+++ src/main/java/nhom8/shoppingweb/ShoppingwebApplication.java
@@ -0,0 +1,13 @@
+package nhom8.shoppingweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingwebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoppingwebApplication.class, args);
+	}
+
+}
