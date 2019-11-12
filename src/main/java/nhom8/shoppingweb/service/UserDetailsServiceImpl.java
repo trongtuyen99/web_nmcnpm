279e0cbc6fe254906161a46faf7e33a7895cf5ad
moonelite99 <56680337+moonelite99@users.noreply.github.com>
2019-11-12 10:21:09 +0700

    test

---
+++ src/main/java/nhom8/shoppingweb/service/UserDetailsServiceImpl.java
@@ -0,0 +1,53 @@
+package nhom8.shoppingweb.service;
+
+/**
+ *
+ * @author Phong
+ */
+ 
+import java.util.ArrayList;
+import java.util.List;
+
+import nhom8.shoppingweb.dao.NguoiDungDAO;
+import nhom8.shoppingweb.entity.NguoiDung;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+ 
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+    /*
+        Tạo đối tượng User từ dữ liệu nhập vào.
+    */
+    
+    @Autowired
+    private NguoiDungDAO nguoiDungDAO;
+ 
+    @Override
+    public UserDetails loadUserByUsername(String userName) throws UsernameNotFoundException {
+        NguoiDung nguoiDung = this.nguoiDungDAO.findUser(userName);
+ 
+        if (nguoiDung == null) {
+            System.out.println("User not found! " + userName);
+            throw new UsernameNotFoundException("User " + userName + " was not found in the database");
+        }
+ 
+        System.out.println("Found User: " + nguoiDung);
+ 
+        List<GrantedAuthority> grantList = new ArrayList<GrantedAuthority>();
+        GrantedAuthority auth = new SimpleGrantedAuthority(nguoiDung.getQuyenTruyCap());
+        grantList.add(auth);
+        
+        // tạo một người dùng mới với tdn, mk và danh sách quyền truy cập.
+        UserDetails userDetails = (UserDetails) new User(nguoiDung.getTenDangNhap(), //
+                nguoiDung.getMatKhau(), grantList);
+ 
+        return userDetails;
+    }
+ 
+}
---
+++ src/main/java/nhom8/shoppingweb/utils/EncryptedPasswordUtils.java
@@ -0,0 +1,27 @@
+package nhom8.shoppingweb.utils;
+
+/**
+ *
+ * @author Phong
+ */
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+ 
+public class EncryptedPasswordUtils {
+    /* 
+        Mã hóa mật khẩu bằng BCryptPasswordEncoder.
+        Khi lưu mật khẩu vào csdl, phải mã hóa trước.
+    */
+    public static String encryptedPassword(String password) {
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+        return encoder.encode(password);
+    }
+ 
+    public static void main(String[] args) {
+        // mã hóa thử
+        String password = "user";
+        String encryptedPassword = encryptedPassword(password);
+ 
+        System.out.println("Encrypted Password: " + encryptedPassword);
+    }
+     
+}
