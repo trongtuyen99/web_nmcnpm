5f0f7374d4820ea63cd4ac0bda69f3e8c542a8e2
Tuyen Dam <damtrongtuyen1999@gmail.com>
2019-11-18 21:49:45 +0700

    add some product.DAO file

---
+++ src/main/java/nhom8/shoppingweb/dao/SanPhamDAO.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nhom8.shoppingweb.dao;
+import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
+import javax.persistence.Query;
+import nhom8.shoppingweb.entity.NguoiDung;
+ 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+ 
+
+/**
+ *
+ * @author damtr
+ */
+@Repository
+@Transactional
+public class SanPhamDAO {
+    public int a;
+    
+    
+}
