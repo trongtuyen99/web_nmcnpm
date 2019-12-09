c978e4a113d23ac3dcc7ead2503fc8cd41f6ced3
moonelite99 <quanvip1999@gmail.com>
2019-12-09 16:53:40 +0700

    done

--- src/main/java/nhom8/shoppingweb/controller/UserController.java
+++ src/main/java/nhom8/shoppingweb/controller/UserController.java
@@ -42,31 +42,7 @@ public class UserController {
                 .orElse("fragments/rfailed");
     }
 
-    @RequestMapping(value = {"/userInfo","/userAccountInfo"}, method = RequestMethod.GET)
-    public String userInfo(Model model, Principal principal) {
 
-        // Sau khi user login thành công, sẽ có principal
-        String userName = principal.getName();
-        System.out.println("User Name: " + userName);
-
-        User loginedUser = (User) ((Authentication) principal).getPrincipal();
-        String userInfo = WebUtils.toString(loginedUser);
-
-        nhom8.shoppingweb.model.User user = userRepository.findByUSERNAME(userName);
-        String USERNAME = user.getUSERNAME();
-        String FULLNAME = user.getFULLNAME();
-        String PHONE = user.getPHONE();
-        String EMAIL = user.getEMAIL();
-        String ROLE = user.getROLE();
-
-        model.addAttribute("userInfo", userInfo);
-        model.addAttribute("USERNAME", USERNAME);
-        model.addAttribute("FULLNAME", FULLNAME);
-        model.addAttribute("PHONE", PHONE);
-        model.addAttribute("EMAIL", EMAIL);
-        model.addAttribute("ROLE", ROLE);
-        return "userInfoPage";
-    }
     // hiển thị danh sách User (đã phân trang)
     @RequestMapping("/listUser")
     public String listUser(Model model,
--- src/main/java/nhom8/shoppingweb/repository/MessageRepository.java
+++ src/main/java/nhom8/shoppingweb/repository/MessageRepository.java
@@ -1,9 +1,15 @@
 package nhom8.shoppingweb.repository;
 
 import nhom8.shoppingweb.model.Message;
+import nhom8.shoppingweb.model.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface MessageRepository extends JpaRepository<Message, Integer> {
+    @Query("SELECT e FROM contactdb e")
+    Page<Message> findAllFeedback(Pageable pageable);
 }
